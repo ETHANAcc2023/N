60fdac436837a76978d9d2c098545f7f84cfde0a
ETHANAcc2023 <ethan.hurkett.754@accesstomusic.ac.uk>
2023-11-22 16:32:41 +0000

    asaa

---
+++ app/Profile/page.js
@@ -0,0 +1,13 @@
+export default function Home() {
+    return (
+        <div>
+            <p>Gib Jhon Tutoring</p>
+
+            <div class="text-start">
+                <p class="ml-4 font-bold text-3xl">Name</p>
+                <div class="bg-white rounded-md text-center mt-6 mx-2 p-4">
+                    <p class="text-lg font-semibold">Achieved Medals</p>
+                </div>
+            </div>
+        </div>
+    )}
